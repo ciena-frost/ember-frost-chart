01f150b565da4e43b4674acc1f417e9d54cd45b2
dafortin@ciena.com <dafortin@ciena.com>
2017-07-11 13:47:08 -0400

    Handle PR comments

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -1,12 +1,12 @@
 /* eslint-disable ember-standard/destructure */
 import Ember from 'ember'
-const {run} = Ember
+const {merge, run} = Ember
 import Application from '../../app'
 import config from '../../config/environment'
 
 export default function startApp (attrs) {
-  let attributes = Ember.merge({}, config.APP)
-  attributes = Ember.merge(attributes, attrs) // use defaults, but you can override;
+  let attributes = merge({}, config.APP)
+  attributes = merge(attributes, attrs) // use defaults, but you can override;
 
   return run(() => {
     let application = Application.create(attributes)
