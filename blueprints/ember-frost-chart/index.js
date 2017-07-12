9ac4c90002f9165b9edf2f0c921feb064c16ca82
dafortin@ciena.com <dafortin@ciena.com>
2017-07-12 09:10:32 -0400

    Handle PR comments

--- blueprints/ember-frost-chart/index.js
+++ blueprints/ember-frost-chart/index.js
@@ -7,7 +7,7 @@ module.exports = {
       packages: [
         {name: 'ember-frost-core', target: '^1.14.3'},
         {name: 'ember-hook', target: '^1.4.1'},
-        {name: 'ember-prop-types', target: '3.11.0'}
+        {name: 'ember-prop-types', target: '^3.11.0'}
       ]
     })
   }
